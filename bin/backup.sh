725a6e0d4749c408c5c027bfdeefaa8d260d63f3
Andrew Spinks <andrewspinks@gmail.com>
2014-06-09 10:53:52 +1000

    Backup music and photos.

--- bin/backup.sh
+++ bin/backup.sh
@@ -9,7 +9,8 @@ echo "" >> $log
 echo "host: $(hostname -f)" >> $log
 echo "" >> $log
 
-rsync -a -R -r --stats --progress --delete --files-from=/Users/andy/backup_list --exclude-from=/Users/andy/backup_exclude /Users/andy/ /Volumes/backup/$host/ >> $log
+# rsync -a -R -r --stats --progress --delete --files-from=/Users/andy/backup_list --exclude-from=/Users/andy/backup_exclude /Users/andy/ /Volumes/backup/$host/ >> $log
+rsync -a -R -r --stats --progress --delete --files-from=/Users/andy/.backup_list /Users/andy/ /Volumes/backup/Backup/ >> $log
 
 echo "" >> $log
 echo "-- END BACKUP $(date)" >> $log
